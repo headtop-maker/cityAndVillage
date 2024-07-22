66a5199c8086ca132c8887bd8a1acdb4f4224354
headtop-maker <headtop@mail.ru>
2024-07-22 11:05:37 +0300

    chore: добавлено распознование текста

--- android/app/src/main/java/com/cityandvillage/CameraView.kt
+++ android/app/src/main/java/com/cityandvillage/CameraView.kt
@@ -1,50 +1,98 @@
 package com.cityandvillage
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.util.AttributeSet
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.facebook.react.uimanager.ThemedReactContext
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.th3rdwave.safeareacontext.getReactContext
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 
-class CameraView(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
+class CameraView(reactContext: ThemedReactContext, attrs: AttributeSet? = null) : FrameLayout(reactContext, attrs) {
 
     private val previewView: PreviewView
     private var imageCapture: ImageCapture?=null
     private val lifecycleOwner: CustomLifecycleOwner = CustomLifecycleOwner()
-
+    private var analysisExecutor: Executor = Executors.newSingleThreadExecutor()
+    
     init {
-//        LayoutInflater.from(context).inflate(R.layout.camera_view, this, true)
-//        previewView = findViewById(R.id.previewView)
-        previewView = PreviewView(context)
+        previewView = PreviewView(reactContext)
         this.addView(previewView)
         startCamera()
-
     }
 
+    private fun sendJsEvent(eventName: String,payload:WritableMap) {
+        val ctx = getReactContext(previewView)
+        ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(eventName,payload)
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
             val preview = Preview.Builder().build()
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
             imageCapture =ImageCapture.Builder().build()
             preview.setSurfaceProvider(previewView.surfaceProvider)
 
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+            imageAnalysis.setAnalyzer(analysisExecutor, ImageAnalysis.Analyzer { imageProxy ->
+                processImageProxy(imageProxy)
+                imageProxy.close()
+            })
+
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview,imageCapture)
+                cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview,imageAnalysis)
                 lifecycleOwner.start()
             } catch (exc: Exception) {
                 Log.e("CameraView", "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(context))
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun processImageProxy(imageProxy: ImageProxy) {
+        val payload = Arguments.createMap()
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+            val result = recognizer.process(image)
+                .addOnSuccessListener { visionText ->
+                    for (block in visionText.textBlocks) {
+                        val blockText = block.text
+//                        val blockCornerPoints = block.cornerPoints
+                        val blockFrame = block.boundingBox
+                        Log.d("TextRecognition", "Recognized text: $blockText , ${blockFrame.toString()} ")
+                        payload.putString("block",blockText)
+                    }
+                    sendJsEvent("sendJsEvent",payload)
+                }.addOnCompleteListener {
+                    imageProxy.close()
+                }
+        }
     }
 
     override fun onDetachedFromWindow() {
--- src/features/CameraX/CameraX.tsx
+++ src/features/CameraX/CameraX.tsx
@@ -1,10 +1,25 @@
-import {requireNativeComponent} from 'react-native';
-import React from 'react';
+import {
+  NativeEventEmitter,
+  NativeModules,
+  requireNativeComponent,
+} from 'react-native';
+import React, {useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 
 const CameraView = requireNativeComponent('CameraView');
 
 const CameraX = () => {
+  useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.CameraView);
+    let eventListener = eventEmitter.addListener('sendJsEvent', event => {
+      console.log('sendJsEvent', JSON.stringify(event, null, 2));
+    });
+
+    // Removes the listener once unmounted
+    return () => {
+      eventListener.remove();
+    };
+  }, []);
   return (
     <View style={styles.container}>
       <CameraView style={styles.camera} />
@@ -19,8 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   camera: {
-    width: '50%',
-    height: '50%',
+    width: '90%',
+    height: '90%',
   },
 });
 

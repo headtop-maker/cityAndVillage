23b4529f2f2d43d51691f001d0cc468265c9171e
VadiuninDA <vadiuninda@lad24.ru>
2024-11-12 10:55:40 +0300

    chore: либа сжатия фото

--- src/features/News/models/models.ts
+++ src/features/News/models/models.ts
@@ -1,9 +1,11 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
+import ImageResizer from '@bam.tech/react-native-image-resizer';
 import {setFileApi} from '../../../shared/api/axiosInstance';
 import {fetchApiDomain} from '../../../shared/constants';
 import {FileParamsType} from '../../../shared/types';
 
 import {nativeFn} from '../../../shared/lib/nativeFn';
+import {Alert} from 'react-native';
 
 export const setFile = createAsyncThunk(
   `${fetchApiDomain}/setFile`,
@@ -12,15 +14,33 @@ export const setFile = createAsyncThunk(
 
     const formData = new FormData();
 
-    formData.append('file', {
-      uri: file.fileUri,
-      type: 'image/jpeg',
-      name: `${file.fileName}.jpg`, // поправить имя
-    });
+    const compressImage = async (uri: string) => {
+      try {
+        const resizedImage = await ImageResizer.createResizedImage(
+          uri, // Путь к исходному изображению
+          1200, // Новая ширина
+          800, // Новая высота
+          'JPEG', // Формат изображения
+          80, // Качество от 0 до 100
+        );
+
+        formData.append('file', {
+          // @ts-ignore
+          uri: resizedImage.uri,
+          type: 'image/jpeg',
+          name: `${file.fileName}.jpg`,
+        });
+      } catch (error) {
+        Alert.alert('Ошибка сжатия изображения', error.toString());
+      }
+    };
+
+    await compressImage(file.fileUri);
 
     try {
       await setFileApi(formData);
     } catch (err) {
+      Alert.alert('Ошибка загрузки изображения', err.toString());
       return rejectWithValue(err);
     }
   },

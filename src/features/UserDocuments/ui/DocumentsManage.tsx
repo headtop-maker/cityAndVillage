3a105f6f8f051057dfafe8236772796c6e41d2be
headtop-maker <headtop@mail.ru>
2024-12-02 18:55:27 +0300

    debug: правки

--- src/features/UserDocuments/ui/DocumentsManage.tsx
+++ src/features/UserDocuments/ui/DocumentsManage.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import AddDocuments from './AddDocuments';
-import {
-  useDeleteDocumentMutation,
-  useGetDocumentsQuery,
-} from '../../../shared/models/services';
+
+import DeleteDocuments from './DeleteDocuments';
 
 const DocumentsManage = () => {
-  const [deleteDocumentId] = useDeleteDocumentMutation();
-  const {data, refetch, isLoading} = useGetDocumentsQuery();
-
-  const handleDelete = async (id: string) => {
-    await deleteDocumentId(id);
-  };
-
   return (
     <View style={styles.container}>
       <AddDocuments />
-      <FlatList
-        refreshing={isLoading}
-        onRefresh={refetch}
-        data={data}
-        keyExtractor={(item, index) => `${item}-${index}`}
-        renderItem={({item}) => (
-          <View style={styles.addedFileItem}>
-            <Text style={styles.text}>{item.documentTitle}</Text>
-            <TouchableOpacity
-              style={styles.deleteButton}
-              onPress={() => handleDelete(item.id)}>
-              <Text style={styles.deleteButtonText}>Удалить</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
+      <DeleteDocuments />
     </View>
   );
 };
@@ -42,37 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#fff',
-  },
-
-  text: {
-    fontSize: 16,
-    color: '#333',
-  },
-
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    borderRadius: 8,
-    marginVertical: 8,
-  },
-  addedFileItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 12,
-    marginVertical: 8,
-    backgroundColor: '#f9f9f9',
-    borderRadius: 8,
-  },
-  deleteButton: {
-    backgroundColor: '#FF4D4D',
-    padding: 8,
-    borderRadius: 8,
-  },
-  deleteButtonText: {
-    color: '#fff',
-    fontSize: 14,
   },
 });
 

13b997ec6b50e713a4f938d00bf2afc020aaa045
VadiuninDA <vadiuninda@lad24.ru>
2025-01-09 10:29:42 +0300

    debug: правки

--- android/app/src/main/java/com/cityandvillage/KotlinModules.kt
+++ android/app/src/main/java/com/cityandvillage/KotlinModules.kt
@@ -387,7 +387,7 @@ public class KotlinModules(reactContext:ReactApplicationContext):ReactContextBas
                     if(bitmap!= null) {
                         val baos = ByteArrayOutputStream()
 
-                        val maxSize = 400
+                        val maxSize = 500
                         val outWidth: Int
                         val outHeight: Int
                         val inWidth: Int = bitmap.width
@@ -402,7 +402,7 @@ public class KotlinModules(reactContext:ReactApplicationContext):ReactContextBas
 
 
                         val scaledBitmap =  Bitmap.createScaledBitmap(bitmap, outWidth,outHeight,true)
-                        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 60, baos)
+                        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos)
                         val b = baos.toByteArray()
                         val encodeImage = Base64.encodeToString(b, Base64.NO_WRAP)
                         fileParams.putString("base64Image", encodeImage.replaceFirst("\"", ""))

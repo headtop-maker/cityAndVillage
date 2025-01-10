24d44e60090d0a939ad8db47be9062515cf16bd3
VadiuninDA <vadiuninda@lad24.ru>
2025-01-10 13:42:26 +0300

    debug: проверка размера изображения

--- src/shared/Hooks/useAddImage.tsx
+++ src/shared/Hooks/useAddImage.tsx
@@ -1,15 +1,22 @@
 import {useState} from 'react';
 import {Alert, Image} from 'react-native';
 import {nativeFn} from '../lib/nativeFn';
+import {base64ByteSize} from '../lib/base64ByteSize';
 
 const useAddImage = () => {
   const [image, setImage] = useState('');
   const [imageSize, setImageSize] = useState({width: 0, height: 0});
   const handleImage = async () => {
     try {
-      const result = await nativeFn.base64Image();
+      const {base64Image} = await nativeFn.base64Image();
+
+      if (base64ByteSize(base64Image) > 95000) {
+        Alert.alert('Ошибка выбора', 'Размер изображения слишком большой');
+        return;
+      }
+
       Image.getSize(
-        `data:image/jpeg;base64,${result.base64Image}`,
+        `data:image/jpeg;base64,${base64Image}`,
         (widthImage, heightImage) => {
           setImageSize({
             width: Math.floor(widthImage / 2),
@@ -17,8 +24,9 @@ const useAddImage = () => {
           });
         },
       );
-      if (result.base64Image) {
-        setImage(result.base64Image);
+
+      if (base64Image) {
+        setImage(base64Image);
       }
     } catch (error) {
       Alert.alert('Ошибка сжатия изображения', error.toString());
